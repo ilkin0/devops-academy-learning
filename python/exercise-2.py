e5d25a97ded4cc0e8d852a846a29d45875eb0f03
ilkin <mehdiyevilkin01@gmail.com>
2021-09-24 17:15:28 +0400

    WIP "exercise-2"

--- python/exercise-2.py
+++ python/exercise-2.py
@@ -1,4 +1,4 @@
-# ex1
+# ex1++
 # inputData = input("Input: ")
 #
 # occ = inputData.count(" ")
@@ -7,14 +7,13 @@
 # ex2
 
 
-# ex3
-
+# ex3++
 # occ = 0
 # string = input("Enter phrase")
 #
 # print(string.replace("is", "was", 2))
 
-# ex4
+# ex4++
 # occ = 0
 # string = input("Enter phrase")
 #
@@ -22,4 +21,44 @@
 #
 # print(string.find("e", firstOcc) + 1)
 
+# ex5++
+# id = input("Enter the number: ")
+#
+# maxNum = 0
+# minNum = 0
+# midNum = 0
+#
+# if int(id[0]) > int(id[1]) and int(id[0]) > int(id[2]):
+#     maxNum = id[0]
+# else:
+#     minNum = id[0]
+#
+# if int(id[1]) > int(id[0]) and int(id[1]) > int(id[2]):
+#     maxNum = id[1]
+# else:
+#     # maxNum = id[2]
+#     if int(minNum) > int(id[1]):
+#         minNum = id[1]
+#     else:
+#         midNum = id[1]
+#
+# if int(id[2]) > int(id[0]) and int(id[2]) > int(id[1]):
+#     maxNum = id[2]
+# else:
+#     if int(minNum) > int(id[2]):
+#         minNum = id[2]
+#     else:
+#         midNum = id[2]
+#
+# if int(maxNum + midNum + minNum) > int(id):
+#     print(maxNum + midNum + minNum)
+# else:
+#     print("The Biggest Number")
+
 # ex6
+# ex7++
+# id = input("Enter the string: ")
+#
+# st = id.split(",")
+
+# ex8
