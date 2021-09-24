ead8ee9a62c710b0e7d62a03e355ddff31587ea3
Ilkin Mehdiyev <ilkin.mehdiyev@cybernet.az>
2021-09-24 16:35:35 +0400

    WIP exercise 2

---
+++ python/exercise-2.py
@@ -0,0 +1,25 @@
+# ex1
+# inputData = input("Input: ")
+#
+# occ = inputData.count(" ")
+# print(occ)
+
+# ex2
+
+
+# ex3
+
+# occ = 0
+# string = input("Enter phrase")
+#
+# print(string.replace("is", "was", 2))
+
+# ex4
+# occ = 0
+# string = input("Enter phrase")
+#
+# firstOcc = string.find("e") + 1
+#
+# print(string.find("e", firstOcc) + 1)
+
+# ex6
